f687a1bddf06ed420659a3154d1c2f17a284e1da
kkallmey <kkallmey@smail.uni-koeln.de>
2025-11-03 16:47:48 +0100

    Text angepasst und Code formatiert

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -6,17 +6,17 @@ public class P1_main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Gib eine Zahl zwischen 1 und 6 ein: ");
-        int wuerfel=input.nextInt();
-        System.out.print("Du hast dich für " + wuerfel +" entschieden und hast ");
+        int wuerfel = input.nextInt();
+        System.out.print("Du hast dich für die Zahl" + wuerfel + " entschieden und hast ");
 
         if (wuerfel == 1 || wuerfel == 4) {
             System.out.println("gewonnen!");
         } else if (wuerfel == 2) {
-            System.out.println("leinder verloren :(");
+            System.out.println("leider verloren :(");
         } else if (wuerfel == 5) {
             System.out.println("unentschieden gespielt");
         } else {
-            System.out.println("ein unbekannts Ergebnis");
+            System.out.println("ein unbekanntes Ergebnis");
         }
     }
 }
