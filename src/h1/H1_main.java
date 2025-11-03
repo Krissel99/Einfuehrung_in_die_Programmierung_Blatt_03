ee29b93d8085c297b04a32ca9f927b83545c6cd2
kkallmey <kkallmey@smail.uni-koeln.de>
2025-11-03 17:14:43 +0100

    H1_main.java Entwurf

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,9 +1,32 @@
 package h1;
 
 public class H1_main {
-	public static void main(String[] args){
+    public static void main(String[] args) {
 
         double guthaben = 0, monEingang = 0;
         int rating = 0;
         boolean warnhinweis, negativ;
+
+        if (guthaben < 0) {
+            negativ = true;
+            if (Math.abs(monEingang) >= guthaben) {
+                rating += 1;
+            }
+            if (Math.abs(monEingang) < guthaben) {
+                rating -= 1;
+            }
+        }else  {
+            negativ = false;
+        }
+        if (guthaben > 0 ) {
+            rating += 3;
+        }
+        if (guthaben == 0 ) {
+            rating += 2;
+        }
+        if (guthaben < 0 && Math.abs(monEingang) < Math.abs(guthaben) && rating == -2 ) {
+            negativ = true;
+            warnhinweis = true;
+        } else warnhinweis = false;
+    }
 }
