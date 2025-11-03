474022b3739228e159ffb678cd8176b8d0ba6839
kkallmey <kkallmey@smail.uni-koeln.de>
2025-11-03 17:28:46 +0100

    H1_main.java formatiert + System.out hinzugefügt

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -3,8 +3,8 @@ package h1;
 public class H1_main {
     public static void main(String[] args) {
 
-        double guthaben = 0, monEingang = 0;
-        int rating = 0;
+        double guthaben = -100, monEingang = 200;
+        int rating = -2;
         boolean warnhinweis, negativ;
 
         if (guthaben < 0) {
@@ -15,18 +15,23 @@ public class H1_main {
             if (Math.abs(monEingang) < guthaben) {
                 rating -= 1;
             }
-        }else  {
+        } else {
             negativ = false;
         }
-        if (guthaben > 0 ) {
+        if (guthaben > 0) {
             rating += 3;
         }
-        if (guthaben == 0 ) {
+        if (guthaben == 0) {
             rating += 2;
         }
-        if (guthaben < 0 && Math.abs(monEingang) < Math.abs(guthaben) && rating == -2 ) {
+        if (guthaben < 0 && Math.abs(monEingang) < Math.abs(guthaben) && rating == -2) {
             negativ = true;
             warnhinweis = true;
         } else warnhinweis = false;
+        System.out.println("Guthaben: " + guthaben);
+        System.out.println("Mon Eingang: " + monEingang);
+        System.out.println("Rating: " + rating);
+        System.out.println("Negativ: " + negativ);
+        System.out.println("Warnhinweis: " + warnhinweis);
     }
 }
