fff01e3911e13dff87d495e76a7ffbf3d1277603
kkallmey <kkallmey@smail.uni-koeln.de>
2025-11-03 17:00:35 +0100

    H1_main angelegt + Variablen definiert

---
+++ src/h1/H1_main.java
@@ -0,0 +1,9 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args){
+
+        double guthaben = 0, monEingang = 0;
+        int rating = 0;
+        boolean warnhinweis, negativ;
+}
