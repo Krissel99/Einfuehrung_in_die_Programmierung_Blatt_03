82a63d93c4ece6a63ae3df6ecd12c974b432ca25
kkallmey <kkallmey@smail.uni-koeln.de>
2025-11-04 18:06:56 +0100

    H3_main.java abgeschlossen

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,6 +2,24 @@ package h3;
 
 public class H3_main {
     public static void main(String[] args) {
+        int max = 5, fix = 2, wartend = 2;
+        boolean istVoll;
 
+        if (fix < max && wartend > 0) {
+            int a = max - fix;
+            if (a > wartend) {
+                fix += wartend;
+                wartend = 0;
+            } else {
+                fix = max;
+                wartend -= a;
+            }
+        }
+
+        istVoll = (fix == max);
+
+        System.out.println("fix = " + fix);
+        System.out.println("wartend = " + wartend);
+        System.out.println("istVoll = " + istVoll);
     }
 }
